abef06460b948241e68810b96c9ec6b87f579d42
Gabriel Conceição <gabrielc2019@outlook.com.br>
2023-04-09 11:47:16 -0300

    correção js

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,5 @@ const menu_opener = document.querySelector('.menu-opener');
 
 menu_opener.addEventListener('click', ()=> {
     const menu = document.querySelector('.menu');
-    console.log( menu.style.display)
     menu.style.display = menu.style.display === 'none' ? 'block' : 'none';
 })
